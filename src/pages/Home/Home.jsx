d590f48275051fa22a5f2bc8fb3e4f835946d58a
dipak <dipakpatil200412@gmail.com>
2025-06-19 11:55:38 +0530

    first commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import './Home.css'
+import Header from '../../components/navbar/Header/Header'
+import ExploreMenu from '../../components/navbar/ExploreMenu/ExploreMenu'
+
+export const Home = () => {
+  const [category,setCategory]= useState("All");
+  return (
+    <div>
+        <Header />
+        <ExploreMenu category={category} setCategory={setCategory} />
+    </div>
+  )
+}
