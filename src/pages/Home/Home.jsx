46f63198b1bf51e46a4ad79c9bb4f5336ae1581e
dipak <dipakpatil200412@gmail.com>
2025-06-20 19:08:10 +0530

    second push

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../components/navbar/Header/Header'
 import ExploreMenu from '../../components/navbar/ExploreMenu/ExploreMenu'
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
 
 export const Home = () => {
   const [category,setCategory]= useState("All");
@@ -9,6 +10,7 @@ export const Home = () => {
     <div>
         <Header />
         <ExploreMenu category={category} setCategory={setCategory} />
+         <FoodDisplay category={category}/>
     </div>
   )
 }
